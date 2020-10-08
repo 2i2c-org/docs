3af8092f8df7a2655f6d9dcdd97467a7de5ab81a
Chris Holdgraf <choldgraf@berkeley.edu>
2020-10-07 17:53:59 -0700

    updating about info

--- conf.py
+++ conf.py
@@ -43,7 +43,7 @@ html_sourcelink_suffix = ""
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ["_static"]
+html_static_path = ["_static"]
 
 html_theme_options = {
     "repository_url": "https://github.com/2i2c-org/pilot",
@@ -82,3 +82,7 @@ hubs_table = f"""
 ```
 """
 Path("hubs-table.txt").write_text(hubs_table)
+
+
+def setup(app):
+    app.add_css_file("custom.css")
