750ea7a29df5455c4cc0f9e7ff722be86f589a5b
Chris Holdgraf <choldgraf@berkeley.edu>
2021-11-18 20:23:19 -0800

    Prototype social image cards

--- conf.py
+++ conf.py
@@ -25,7 +25,15 @@ templates_path = ["_templates"]
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "README.md", ".github", ".nox", "CONTRIBUTING.md"]
+exclude_patterns = [
+    "_build",
+    "Thumbs.db",
+    ".DS_Store",
+    "README.md",
+    ".github",
+    ".nox",
+    "CONTRIBUTING.md",
+]
 
 myst_enable_extensions = ["colon_fence", "deflist", "linkify"]
 
@@ -54,14 +62,13 @@ html_theme_options = {
 }
 
 intersphinx_mapping = {
-    "tc": ('https://team-compass.2i2c.org/en/latest', None),
-    "infra": ('https://infrastructure.2i2c.org/en/latest', None),
-    "jb": ('https://jupyterbook.org', None),
-    "z2jh": ('https://z2jh.jupyter.org/en/latest', None),
+    "tc": ("https://team-compass.2i2c.org/en/latest", None),
+    "infra": ("https://infrastructure.2i2c.org/en/latest", None),
+    "jb": ("https://jupyterbook.org", None),
+    "z2jh": ("https://z2jh.jupyter.org/en/latest", None),
 }
 
-rediraffe_redirects = {
-}
+rediraffe_redirects = {}
 
 # Disable linkcheck for anchors because it throws false errors for any JS anchors
 linkcheck_anchors = False
@@ -74,6 +81,7 @@ def setup(app):
 # Scripts to run
 import subprocess
 from pathlib import Path
+
 build_assets = Path("build_assets")
 build_assets.mkdir(exist_ok=True)
 subprocess.run(["python", "feature-table.py"], cwd="scripts")
@@ -84,17 +92,18 @@ from matplotlib import pyplot as plt
 import matplotlib.image as mpimg
 from docutils.nodes import paragraph
 
+
 def create_summary_card(app, pagename, templatename, context, doctree):
     if not doctree:
         return
-    # +
     site_title = context.get("docstitle", "")
     page_title = context.get("title", "")
     text = " ".join([ii.astext() for ii in doctree.traverse(paragraph)])
-    N_CHAR_DESCRIPTION = 50
-    tagline = " ".join(text.split()[:N_CHAR_DESCRIPTION])
+    N_WORDS_DESCRIPTION = 30
+    tagline = " ".join(text.split()[:N_WORDS_DESCRIPTION]) + "..."
     logo = "./images/logo.png"
     img = mpimg.imread(logo)
+    font = "Roboto"
 
     # Colors
     # background_color = "#7e7e7e"
@@ -108,35 +117,56 @@ def create_summary_card(app, pagename, templatename, context, doctree):
     multiple = 3
 
     # +
-    fig = plt.figure(figsize=(ratio*multiple, multiple))
+    fig = plt.figure(figsize=(ratio * multiple, multiple))
     fig.set_facecolor(background_color)
 
-    left_margin = .05
+    left_margin = 0.05
 
-    with plt.rc_context({'font.sans-serif': ["Roboto"], "text.color": text_color}):
+    with plt.rc_context({"font.sans-serif": [font], "text.color": text_color}):
         axtext = fig.add_axes((0, 0, 1, 1))
-        txt_title = axtext.text(left_margin, .90, site_title, {"size":20,}, ha='left', va='top', wrap=True)
-        txt_page = axtext.text(left_margin, .7, page_title, {"size":25, "fontweight": "bold"}, ha='left', va='top', wrap=True)
-        txt_page._get_wrap_line_width = lambda : 300
+        txt_title = axtext.text(
+            left_margin,
+            0.90,
+            site_title,
+            {
+                "size": 20,
+            },
+            ha="left",
+            va="top",
+            wrap=True,
+        )
+        txt_page = axtext.text(
+            left_margin,
+            0.75,
+            page_title,
+            {"size": 26, "fontweight": "bold"},
+            ha="left",
+            va="top",
+            wrap=True,
+        )
+        txt_page._get_wrap_line_width = lambda: 400
 
-        txt_description = axtext.text(left_margin, .25, tagline, {"size":15}, ha='left', va='top', wrap=True)
-        txt_description._get_wrap_line_width = lambda : 400
+        txt_description = axtext.text(
+            left_margin, 0.35, tagline, {"size": 13}, ha="left", va="top", wrap=True
+        )
+        txt_description._get_wrap_line_width = lambda: 500
         axtext.set_axis_off()
 
-    axim = fig.add_axes((.8, .7, .2, .2))
+    axim = fig.add_axes((0.8, 0.8, 0.15, 0.15))
     axim.imshow(img)
     axim.set_axis_off()
 
     from pathlib import Path
-    static_dir = Path(app.builder.outdir) / '_static'
+
+    static_dir = Path(app.builder.outdir) / "_static"
     static_dir.mkdir(exist_ok=True)
     path_out = f"summary_{pagename.replace('/', '_')}.png"
     fig.savefig(static_dir / path_out)
-    n_depth = len(pagename.split('/')) - 1
+    n_depth = len(pagename.split("/")) - 1
     path_out_image = f"{'../' * n_depth}_static/{path_out}"
-    context["metatags"] += f'\n    <meta property="og:image" content="{path_out_image}" />'
-
-
+    context[
+        "metatags"
+    ] += f'\n    <meta property="og:image" content="{path_out_image}" />'
 
 
 # ==================================
