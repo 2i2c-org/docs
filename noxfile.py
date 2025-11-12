81621d908877e058bc092011eabb8b07f4ff3e83
Chris Holdgraf <choldgraf@gmail.com>
2025-11-11 20:10:45 -0800

    Document mounting repositories (#280)

--- noxfile.py
+++ noxfile.py
@@ -7,17 +7,21 @@ build_command = ["-b", "dirhtml", ".", "_build/dirhtml"]
 
 @nox.session()
 def docs(session):
+    """Build the documentation with sphinx-build"""
     session.install("-r", "requirements.txt")
-    if "live" in session.posargs:
-        AUTOBUILD_IGNORE = [
-            "_build",
-            "build_assets",
-            "images/shared_responsibility_diagram.png",
-        ]
-        cmd = ["sphinx-autobuild"]
-        for folder in AUTOBUILD_IGNORE:
-            cmd.extend(["--ignore", f"*/{folder}/*"])
-        cmd.extend(build_command)
-        session.run(*cmd)
-    else:
-        session.run("sphinx-build", *build_command)
+    session.run("sphinx-build", *build_command)
+
+@nox.session(name="docs-live")
+def docs_live(session):
+    """Build the documentation with sphinx-autobuild for live preview"""
+    session.install("-r", "requirements.txt")
+    AUTOBUILD_IGNORE = [
+        "_build",
+        "build_assets",
+        "images/shared_responsibility_diagram.png",
+    ]
+    cmd = ["sphinx-autobuild"]
+    for folder in AUTOBUILD_IGNORE:
+        cmd.extend(["--ignore", f"*/{folder}/*"])
+    cmd.extend(build_command)
+    session.run(*cmd)
