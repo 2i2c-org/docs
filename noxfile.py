db74f5208dd0cd8263c83e18d64d49cfc8db3edb
choldgraf <choldgraf@gmail.com>
2026-07-28 11:11:53 +0200

    Remove sphinx config and split stuff out into myst config and scripts

--- noxfile.py
+++ noxfile.py
@@ -3,25 +3,18 @@ import nox
 nox.options.default_venv_backend = "uv"
 nox.options.reuse_existing_virtualenvs = True
 
-build_command = ["-b", "dirhtml", ".", "_build/dirhtml"]
 
 @nox.session()
 def docs(session):
-    """Build the documentation with sphinx-build"""
-    session.install("-r", "requirements.txt")
-    session.run("sphinx-build", *build_command)
+    """Build the documentation."""
+    session.install("mystmd")
+    session.run("python", "scripts/download-images.py")
+    session.run("myst", "build", "--html", *session.posargs)
+
 
 @nox.session(name="docs-live")
 def docs_live(session):
-    """Build the documentation with sphinx-autobuild for live preview"""
-    session.install("-r", "requirements.txt")
-    AUTOBUILD_IGNORE = [
-        "_build",
-        "build_assets",
-        "images/shared_responsibility_diagram.png",
-    ]
-    cmd = ["sphinx-autobuild"]
-    for folder in AUTOBUILD_IGNORE:
-        cmd.extend(["--ignore", f"*/{folder}/*"])
-    cmd.extend(build_command)
-    session.run(*cmd)
+    """Build the documentation with live preview server."""
+    session.install("mystmd")
+    session.run("python", "scripts/download-images.py")
+    session.run("myst", "start", *session.posargs)
